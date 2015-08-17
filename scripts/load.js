f74a855519fa01f8696b49ad03c9c312471a7321
Oliver De Bruin <oliverdb@hotmail.co.za>
2015-08-17 08:55:03 +0200

    added a comment

--- scripts/load.js
+++ scripts/load.js
@@ -2,10 +2,11 @@ var preLoadState = {
 	preload: function(){
 		time = game.time.now;
 		loadingBar = game.add.sprite(game.width/2 - game.cache.getImage('loadingBar').width/2, game.height/2, 'loadingBar');
-		//loadingBar.
+		//anchor		
 		game.load.setPreloadSprite(loadingBar);
 		var loadingImage = game.add.sprite(game.width/2 - game.cache.getImage('loadingText').width/2, game.height/2 - loadingBar.height, 'loadingText');
 
+		//images
 		game.load.image('plus', 'assets/plus.png');	
 		game.load.image('background', 'assets/starBackground.png');
 		game.load.image('asteroid_big', 'assets/meteorBig.png');
@@ -14,7 +15,12 @@ var preLoadState = {
 		game.load.image('title', 'assets/title.png');
 		game.load.image('howto', 'assets/howto.png');
 		game.load.image('howtoplay', 'assets/howtoplay.png');
+
+		//spritesheets
 		game.load.spritesheet('space_ship', 'assets/player.png',100,100,4);
+
+
+		//audio
 		game.load.audio('audio', "assets/sound/rocket.mp3");
 		game.load.audio('blast', "assets/sound/blast.mp3");
 		game.load.audio('end', "assets/sound/end.mp3");
