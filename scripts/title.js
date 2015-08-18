b2934c8064cec78e686609e2d4a25a05380801d6
Yepster <CarySmall2295@gmail.com>
2015-08-18 12:07:49 +0200

    Asteriod progress

--- scripts/title.js
+++ scripts/title.js
@@ -1,18 +1,17 @@
-titleState = {	
-	
+titleState = {		
 	create: function(){
-		theme = game.add.sound('theme', 0.5, true);
-		debugMode = false;
+		theme            = game.add.sound('theme', 0.5, true);
+		debugMode        = false;
 		theme.play();
-		var gameTitle = game.add.button(game.width/2, game.height/3, 'title', this.playClicked, this);
-		var debugButton = game.add.button(game.width, game.height, 'debug',  this.playDebug, this);
+		var gameTitle    = game.add.button(game.width/2, game.height/3, 'title', this.playClicked, this);
+		var debugButton  = game.add.button(game.width, game.height, 'debug',  this.playDebug, this);
 		debugButton.anchor.setTo(1, 1);
 		gameTitle.anchor.setTo(0.5, 0.5);
-		var howTo = game.add.sprite(gameTitle.x, gameTitle.y + gameTitle.height, 'howto');
+		var howTo        = game.add.sprite(gameTitle.x, gameTitle.y + gameTitle.height, 'howto');
 		howTo.anchor.set(0.5, 0.5);
 		var instructions = game.add.sprite(howTo.x, howTo.y + 2*(howTo.height), 'howtoplay');
 		instructions.anchor.set(0.5, 0.5);
-		var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+		var enterKey     = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		enterKey.onDown.addOnce(this.playClicked, this);
 	},
 
